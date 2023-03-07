ecfd50ed3e644a03e6e84ec81d9bdb9caa6f3b84
Andreia Costa <andreia_a.c@msn.com>
2023-03-06 23:14:20 -0300

    adding login route

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,12 @@ import {
   Route
 } from "react-router-dom";
 import { Home } from "./pages/Home";
+import Login from "./pages/Login";
 
 export function RoutesComponent() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />      <Route path="/" element={<Login />} />
     </Routes>
   )
 } 
